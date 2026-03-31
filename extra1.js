6efcdaeeb7d96510b675774ebc5b4a268366d92a
StemWebsite <apedada2012@gmail.com>
2026-03-31 14:27:00 -0400

    Add files via upload

---
+++ extra1.js
@@ -0,0 +1,17 @@
+const paragraphs = document.querySelectorAll(".small-timeline-text");
+
+document.addEventListener("scroll", function(){
+    paragraphs.forEach(paragraph => {
+        if(isInView(paragraph)){
+            paragraph.classList.add
+            ("small-timeline-text--visible")
+        }
+    })
+})
+
+function isInView(element){
+    const rect = element.getBoundingClientRect();
+    return (
+    rect.bottom > 0 && rect.top < (window.innerHeight - 150 || document.documentElement.clientHeight - 150)
+    );
+}
